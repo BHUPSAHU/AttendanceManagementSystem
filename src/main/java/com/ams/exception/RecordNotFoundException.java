aa94043a821413a0d4eb2eaf2bd16e4063d26990
Aishwaryak909 <aishwarya.b.k@capgemini.com>
2021-08-14 09:34:19 +0530

    Course-testcase,validation all done
    course testing

--- src/main/java/com/ams/exception/RecordNotFoundException.java
+++ src/main/java/com/ams/exception/RecordNotFoundException.java
@@ -4,8 +4,6 @@ package com.ams.exception;
 @SuppressWarnings("serial")
 public class RecordNotFoundException extends Exception 
 {
-	
-//	private static final long serialVersionUID = 1L;
 
 	public RecordNotFoundException(String msg) {
 		super(msg);
